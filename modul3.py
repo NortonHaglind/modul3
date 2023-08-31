579e67ff6a71bdd45397d576b5fb2154c6e2ffa9
NortonHaglind <norton.haglindhilbers@elev.ga.ntig.se>
2023-08-31 12:45:02 +0200

    first commit

---
+++ modul3.py
@@ -0,0 +1,34 @@
+x=str(input("vad är ditt favorit ord?: ")) 
+for i in range (10):
+    print(x)
+for i in range(0, 10):
+    print(i+1)
+y=int(input("säg ett tal: "))# räkna till ditt tal
+for i in range(0, y):
+    print(i+1)
+for table in range(1, 13): #räkne tabell
+    print(f"{table}'ans TABELL")
+    for z in range(1,13):
+        print(f"{table} * {z} = {str(table * z)}")
+
+
+
+#optionell
+
+
+'''
+först loopa med for hur många ggr man ska höja upp
+    hur mycket ska det höjas med ,inst 2
+
+
+
+'''
+'''x=int(input("vad ska upphöjas?: "))
+exp=int(input("hur mycket ska det upphöjas med?: "))
+
+sum = 0
+for i in range(exp):
+    while True:
+        x*x
+        print(x*x) '''#orkar inte räkna ut
+        
